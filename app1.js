7af77f1d858eb6aeb90a7e72e467018cc63dc76b
Garinmin <garinmin@gmail.com>
2021-02-24 19:52:03 -0600

    Added js

---
+++ app1.js
@@ -0,0 +1,14 @@
+let userPref = confirm('Do you like a music?');
+if (userPref == true){ 
+}
+else{
+document.write('<h2>' + 'Maybe you can look for another site' + '</h2>');
+}
+let firstName = prompt('Please enter your name: ');
+let firstVisit = confirm('Are you first time in this site?');
+if (firstVisit == true){
+    document.write('<h2>' + 'Congratulations on your first visit, ' + firstName + '</h2>');
+} 
+else{
+    document.write('<h2>' + 'Welcome back, ' + firstName + '</h2>');
+}
